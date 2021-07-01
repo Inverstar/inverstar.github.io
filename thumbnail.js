5c32457c295421a01d87868b8ff21224a4c62ab4
Inverstar <44256495+Inverstar@users.noreply.github.com>
2021-07-01 13:20:15 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   
     // 听好了,你的代码都必须写在下面!
   
-  alert("Hello javascript");
+  //alert("Hello javascript");
   let targetImage = document.querySelector("#smart-image");
   targetImage.addEventListener
   (
